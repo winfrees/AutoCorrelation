b0a473d9154ae0844504436a1809171cc9c16885
winfrees <vinfrais@yahoo.com>
2016-02-02 07:34:15 -0500

    Syncing to new master

--- AutoCorrelationPlayground/src/main/java/edu/iu/icbm/autocorrelationplayground/TestFFT.java
+++ AutoCorrelationPlayground/src/main/java/edu/iu/icbm/autocorrelationplayground/TestFFT.java
@@ -16,6 +16,7 @@ package edu.iu.icbm.autocorrelationplayground;
 import edu.emory.mathcs.jtransforms.fft.DoubleFFT_1D;
 import java.util.Arrays;
 import javax.swing.JFrame;
+import java.util.Random;
 
 public class TestFFT extends JFrame {
 
@@ -46,16 +47,17 @@ public class TestFFT extends JFrame {
         int n = x.length;
         // Assumes n is even.
         DoubleFFT_1D fft = new DoubleFFT_1D(n);
-        fft.realForward(x);
-        ac[0] = sqr(x[0]);
+        fft.realForward(x); //real forwards fft of data
+        ac[0] = sqr(x[0]); //square of fft of data is the first result
+        //square is an auto-correlation
         //ac[0] = 0;  // For statistical convention, zero out the mean 
-        ac[1] = sqr(x[1]);
-        for (int i = 2; i < n; i += 2) {
-            ac[i] = sqr(x[i]) + sqr(x[i+1]);
-            ac[i+1] = 0;
+        ac[1] = sqr(x[1]);  //square of fft of data is the second result
+        for (int i = 2; i < n; i += 2) {// step through n from 3rd every second (correlation)
+            ac[i] = sqr(x[i]) + sqr(x[i+1]); // square of current fft plus the square of next fft
+            ac[i+1] = 0; // next result is 0 (skipped over by n += 2
         }
         DoubleFFT_1D ifft = new DoubleFFT_1D(n); 
-        ifft.realInverse(ac, true);
+        ifft.realInverse(ac, true);//inverse the result ac 
         // For statistical convention, normalize by dividing through with variance
 //        for (int i = 1; i < n; i++)
 //            ac[i] /= ac[0];
@@ -78,8 +80,8 @@ public class TestFFT extends JFrame {
         stop = System.currentTimeMillis();
         System.out.println("FFT time: " + (stop-start) + " ms.");
         //print("data", data);
-        print("bf", ac1);
-        print("fft", ac2);
+        //print("bf", ac1);
+        //print("fft", ac2);
         
         double err = 0;
         for (int i = 0; i < ac1.length; i++)
@@ -105,8 +107,9 @@ public class TestFFT extends JFrame {
         
          System.out.println("Generated data: ");
         for(int i = 0; i < n*2; i++){   
+            Random r = new Random(System.nanoTime());
             double radian = (Math.PI/200)*i;
-            sine[i] = Math.sin(radian);
+            sine[i] =poissonValue(Math.sin(radian)+5)+Math.sin(radian)+5;
             System.out.println(sine[i]);
     }       
     return sine;    
@@ -115,5 +118,20 @@ public class TestFFT extends JFrame {
     public class Diagram extends JFrame{
         
     }
+    
+    //http://rsb.info.nih.gov/ij/plugins/download/Poisson_Noise.java
+    
+    private double poissonValue(double pixVal) {
+      Random random = new Random();
+      double L = Math.exp(-(pixVal));
+      int k = 0;
+      double p = 1;
+      do {
+         k++;
+         // Generate uniform random number u in [0,1] and let p ← p × u.
+         p *= random.nextDouble();
+      } while (p >= L);
+      return (k - 1);
+   }
     
 }
